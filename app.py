8378b00d96075746554d7b83ca33ca2f29ddc559
Sharan9277 <91002344+Sharan9277@users.noreply.github.com>
2025-02-14 03:16:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -149,4 +149,5 @@ def calculate_z_test():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if not set
+    app.run(host='0.0.0.0', port=port)
